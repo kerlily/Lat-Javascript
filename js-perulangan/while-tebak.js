949a32dc7bbbc2b5b64a7063fabcbcbb7bfa2015
kerlily <farhan15062004@gmail.com>
2024-12-11 22:34:56 +0700

    feat studi kasus tebak tebakan ankga menggunakan while

--- js-perulangan/while-tebak.js
+++ js-perulangan/while-tebak.js
@@ -1,37 +1,3 @@
-// let num = 0;
-// while (num < 10) {
-//     console.log(num);
-//     num++;
-// }
-
-// const password = 'pass123';
-
-// let guess = prompt('masukkan password');
-
-// while (guess !== password) {
-//     guess = prompt('masukkan password');
-// }
-// alert('password benar');    
-
-
-// let input = prompt('say something');
-// while(true){
-//     input = prompt(input);
-//     if(input.toLowerCase() === 'stop') 
-//         break;
-    
-// }
-// alert('stopped');
-
-// for (let i = 0; i < 1000; i++) {
-//     if (i === 100)
-//     {
-//         break;
-//     }
-//     console.log(i);
-// }
-
-
 let maximum = parseInt(prompt('masukkan nilai maximal: '));
 while(!maximum) {
     maximum = parseInt(prompt('masukkan nilai maximal: '));
