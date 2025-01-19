fb58e4a72a433685168524adb7ef8d357188be91
kerlily <farhan15062004@gmail.com>
2025-01-19 20:27:29 +0700

    bikin palindrom

---
+++ dewe/palindrom.js
@@ -0,0 +1,10 @@
+const palindrom = (word) => {
+    const reverse = word.toLowerCase().split("").reverse().join("");
+    if (reverse === word.toLowerCase()) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(palindrom('susah hasus'));
+
