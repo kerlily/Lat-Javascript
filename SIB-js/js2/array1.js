eeac968f7ecc1d4eeb9911913bcf0a7234633be5
kerlily <farhan15062004@gmail.com>
2025-03-18 23:20:31 +0700

    learn array again

---
+++ SIB-js/js2/array1.js
@@ -0,0 +1,17 @@
+let nomor = [4,2,2,4]
+let dowo = nomor.length;
+
+
+if(nomor.some(num => num % 2 !== 0)){
+    console.log("ganjil")
+}
+else{
+    console.log("genap")
+}
+
+// if (dowo % 2 === 0) {
+//     console.log("index genep")
+// } else {
+//     console.log("index ganjkil")
+// }
+
